08bea3658dd66e2bd896d04836904c051e65efa1
Pakizamutallim <127186291+Pakizamutallim@users.noreply.github.com>
2023-05-12 13:35:13 +0400

    Add files via upload

---
+++ fiorello Image Filter/assets/js/script.js
@@ -0,0 +1,82 @@
+const item = document.querySelector(".item");
+const cactusLi = document.getElementById("cactus");
+const exotic = document.querySelector(".exotic");
+const cactus = document.querySelector(".cactus")
+
+// cactus.style.display = "block"
+
+cactusLi.addEventListener("click", ()=>{
+    exotic.style.display = "none";
+    // cactus.style.display = "block"
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var flowers = [
+//     {
+//         img : "https://cdn.shopify.com/s/files/1/2045/8185/products/Majesty-palm-tree_600x600.jpg?v=1613582423",
+//         name : "Majesty palm",
+//         price : "$300"
+//     },
+//     {
+//         img : "https://houseplantresourcecenter.com/wp-content/uploads/2020/06/AfterlightImage-1.jpg",
+//         name : "",
+//         price : "$250"
+//     },
+//     {
+//         img : "https://cdn.shopify.com/s/files/1/0150/6262/products/the_sill-variant-white_gloss-orchid_white.jpg?v=1681923981",
+//         name : "Orchide",
+//         price : "$155"
+//     },
+//     {
+//         img : "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQF0shOaf5G14DTc2G956sc58yk_dti6SY3-g&usqp=CAU",
+//         name : "Soapworth",
+//         info : "$190"
+//     },
+    
+// ]
+
+// for (let i = 0; i < flowers.length; i++) {
+//     area.innerHTML +=`<div class="item">
+//     <div class="flower"
+//         <img src="${flowers[i].img}" width = "180px" height="240px" alt="">
+//     </div>
+//     <div class="names">
+//         <p>${flowers[i].name}</p>
+//         <span>${flowers[i].price}</span>
+//     </div>
+// </div>`
+// }
+
+
+
+
+
+
+
+
+
+
+
+// const register = document.querySelector(".register"); 
+// const username = document.getElementById("username");
+// const password = document.getElementById("password");
+
+// register.addEventListener ("click", ()=>{ 
+//     var user = JSON.parse(localStorage.getItem("users")); 
+//     localStorage.setItem("username", username.value); 
+//     localStorage.setItem("password", password.value);
+// })
+
+
+
